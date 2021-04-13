e87c9c75be2b772488c4c9adbe957c2aa8ba095c
tormeapp <81472103+tormeapp@users.noreply.github.com>
2021-04-13 13:26:46 +0300

    Add files via upload

---
+++ components/Login.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Search from './Search';
+
+
+function Login({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Search')}
+      />
+    </View>
+  );
+}
+
+export default Login;
---
+++ components/Search.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function Search({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.navigate('Search')}
+        />
+      </View>
+    );
+  }
+
+  export default Search;
