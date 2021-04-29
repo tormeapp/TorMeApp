e65350211c076533bb9cccc0b917d2782f0228a6
rolan <rolan.liev@gmail.com>
2021-04-29 21:11:58 +0300

    Search.js , SignUp.js
    Style testing....

--- screens/Search.js
+++ screens/Search.js
@@ -1,41 +1,73 @@
-import React, { useContext } from 'react';
-import { Button, View, Text, StyleSheet, TextInput } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import DatePicker from '../components/DatePicker/DatePicker';
-import { AuthContext } from '../navigation/AuthProvider';
+import React, { useContext } from "react";
+import {
+  Button,
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import DatePicker from "../components/DatePicker/DatePicker";
+import { AuthContext } from "../navigation/AuthProvider";
+import { KeyboardAvoidingView } from "react-native";
+import { Input, Image } from "react-native-elements";
 
-function Search({ navigation }) { 
-    const { user, logout } = useContext(AuthContext);
-    return (
-      <View style={styles.container}>
-        <StatusBar style="auto"/>
-        <View style={styles.searchbar}>
-        <Text style={styles.text}>HomePage, Welcome {user.email.slice(0, user.email.indexOf('@'))}</Text>
-        <TextInput placeholder='Search for anything..'></TextInput>
+function Search({ navigation }) {
+  const { user, logout } = useContext(AuthContext);
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={{ margin: 30, color: "white" }}>
+          HomePage, Welcome {user.email.slice(0, user.email.indexOf("@"))}
+        </Text>
+        <View style={styles.inputContainer}>
+          <Input
+            placeholder="Search for anything.."
+            leftIcon={{ type: "font-awesome", name: "search" }}
+          />
         </View>
-        <Button style={styles.logoutBtn} onPress={() => logout()} title='Logout'></Button>
-        </View>
-    );
-  }
+      </View>
+      <TouchableOpacity
+        style={styles.logoutBtn}
+        onPress={() => logout()}
+        style={styles.logOutButton}
+      >
+        <Text>Log Out</Text>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
+  );
+}
 
-  export default Search;
+export default Search;
 
-  const styles = StyleSheet.create({
-     container: {
-      flex: 1,
-      alignItems: 'center',
-     justifyContent: 'center',
-     padding: 10
-     },
-     text:{
-       padding: 20,
-       margin: 5
-     },
-     searchbar:{
-       flex: 2,
-       padding: 10
-     },
-     logoutBtn:{
-       flex: 1
-     }
-   });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "#0081a7",
+  },
+  inputContainer: {
+    marginBottom: 20,
+    width: 300,
+    padding: 10,
+    backgroundColor: "#fdfcdc",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  header: {
+    alignItems: "center",
+    flex: 1,
+  },
+  logOutButton: {
+    width: 250,
+    alignItems: "center",
+    backgroundColor: "#ff595e",
+    padding: 10,
+    borderRadius: 7,
+    margin: 30,
+  },
+});
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,68 +1,118 @@
-import React, { useContext, useState } from 'react';
-import { AuthContext } from '../navigation/AuthProvider';
-import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import fb from '../fb';
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../navigation/AuthProvider";
+import { View, TextInput, Button, Text, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import fb from "../fb";
+import { KeyboardAvoidingView } from "react-native";
+import { Input, Image } from "react-native-elements";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const SignUp = ({ navigation }) => {
-    const { register } = useContext(AuthContext);
-    const [displayName, setDisplayName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [emailError, setEmailError] = useState("");
-    const [passwordError, setPasswordError] = useState("");
-    const [hasAccount, setHasAccount] = useState(true);
+  const { register } = useContext(AuthContext);
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(true);
 
+  const handleSignUp = (email, password) => {
+    clearErrors();
+    fb.auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        //   fb.database.
+        navigation.navigate("Login");
+      })
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+            setEmailError("Enter a valid email");
+            break;
+          case "auth/email-already-in-use":
+            setEmailError(
+              "This email is already registered.\nEnter a different email"
+            );
+            break;
+          case "auth/weak-password":
+            setPasswordError("Password length should be at least 8 characters");
+            break;
+        }
+      });
+  };
 
-    const handleSignUp = (email, password) => {
-      clearErrors();
-      fb
-          .auth()
-          .createUserWithEmailAndPassword(email, password)
-          .then(() =>{
-          //   fb.database.
-          navigation.navigate('Login')
-          })
-          .catch((err) => {
-              switch (err.code) {
-                  case "auth/invalid-email":
-                      setEmailError('Enter a valid email');
-                      break;
-                  case "auth/email-already-in-use":
-                      setEmailError('This email is already registered.\nEnter a different email');
-                      break;
-                  case "auth/weak-password":
-                      setPasswordError('Password length should be at least 8 characters');
-                      break;
-              }
-          });
-    };
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          onChangeText={(email) => setEmail(email)}
+          leftIcon={{ type: "font-awesome", name: "envelope" }}
+        />
+        <Text>{emailError}</Text>
+        <Input
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+        />
+        <Input
+          placeholder="Confirm Password"
+          secureTextEntry={true}
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+        />
+        <Text>{passwordError}</Text>
+        <TouchableOpacity
+          onPress={() => register(email, password)}
+          style={styles.signUpButton}
+        >
+          <Text>Sign Up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.loginButton}
+        >
+          <Text>Already have an account? Log In</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
-
-    return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-       <StatusBar style="auto" />
-      <Text>Sign Up</Text>
-      <TextInput
-        placeholder="Email"
-        onChangeText={(email) => setEmail(email)}
-      />
-      <Text>{emailError}</Text>
-      <TextInput
-        placeholder="Password"
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Text>{passwordError}</Text>
-      <Button
-        title="Sign Up"
-        onPress={() => register(email, password)}
-      />
-      <Button
-        title="Already have an account? Log In"
-        onPress={() => navigation.goBack()}
-      />
-    </View>);
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "#0081a7",
+  },
+  signUpButton: {
+    width: 250,
+    alignItems: "center",
+    backgroundColor: "#8ac926",
+    padding: 10,
+    borderRadius: 7,
+    margin: 10,
+  },
+  loginButton: {
+    width: 250,
+    alignItems: "center",
+    backgroundColor: "#ff595e",
+    padding: 10,
+    borderRadius: 7,
+    margin: 10,
+  },
+  inputContainer: {
+    marginBottom: 20,
+    width: 350,
+    height: 400,
+    padding: 10,
+    backgroundColor: "#fdfcdc",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+});
 
 export default SignUp;
