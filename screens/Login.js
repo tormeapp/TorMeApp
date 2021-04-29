cf5c3351e0cf21d30dea8cd73769f9560563f842
rolan <therolan420@gmail.com>
2021-04-29 20:09:20 +0300

    Login Style test

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,10 @@
-import React, { useState, useContext } from 'react';
-import {StyleSheet, Button, View, Text, TextInput, TouchableOpacity } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { AuthContext } from '../navigation/AuthProvider';
-
+import React, { useState, useContext } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { AuthContext } from "../navigation/AuthProvider";
+import { KeyboardAvoidingView } from "react-native";
+import { Input, Image } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 function Login({ navigation }) {
   const { login } = useContext(AuthContext);
@@ -12,45 +14,85 @@ function Login({ navigation }) {
   const [password, setPassword] = useState("");
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
       <StatusBar style="auto" />
-      <Text>Home Screen</Text>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={(email) => setEmail(email)}
-        keyboardType='email-address'
-        autoCapitalize='none'
-        autoCorrect={false}
+      <Image
+        source={require("../assets/Logo.png")}
+        style={{ width: 300, height: 100, marginBottom: 90 }}
       />
-      <Text>{emailError}</Text>
-      <TextInput
-        placeholder="Password"
-        value={password}
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Text>{passwordError}</Text>
-      <Button
-        title="Login"
-        onPress={() => login(email, password)}
-      />
-      <TouchableOpacity
-        onPress={() => navigation.navigate('SignUp')}
-      >
-        <Text>New User? Sign Up</Text>
-      </TouchableOpacity>
-    </View>
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          leftIcon={{ type: "font-awesome", name: "envelope" }}
+          autoFocus
+          type="Email"
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text>{emailError}</Text>
+        <Input
+          placeholder="Password"
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+          type="Password"
+          value={password}
+          secureTextEntry
+          onChangeText={(password) => setPassword(password)}
+        />
+        <Text>{passwordError}</Text>
+        <TouchableOpacity
+          onPress={() => login(email, password)}
+          style={styles.loginButton}
+        >
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("SignUp")}
+          style={styles.signUpButton}
+        >
+          <Text>SignUp</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    padding: 20,
-    alignItems: 'center',
-    justifyContent: 'center' 
-  }
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "#0081a7",
+  },
+
+  // Testing diff buttons working on diff levels
+  loginButton: {
+    width: 250,
+    alignItems: "center",
+    backgroundColor: "#8ac926",
+    padding: 10,
+    borderRadius: 7,
+    margin: 10,
+  },
+  signUpButton: {
+    width: 250,
+    alignItems: "center",
+    backgroundColor: "#ff595e",
+    padding: 10,
+    borderRadius: 7,
+    margin: 10,
+  },
+  inputContainer: {
+    marginBottom: 20,
+    width: 350,
+    height: 350,
+    padding: 10,
+    backgroundColor: "#fdfcdc",
+    borderRadius: 10,
+    alignItems: "center",
+  },
 });
 
 export default Login;
