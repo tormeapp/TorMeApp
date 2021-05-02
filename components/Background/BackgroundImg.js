5c9ba66f48d5b2d446e251fa274ec9d967808473
rolan <rolan.liev@gmail.com>
2021-05-02 11:41:16 +0300

    Update BackgroundImg.js

--- components/Background/BackgroundImg.js
+++ components/Background/BackgroundImg.js
@@ -2,21 +2,9 @@ import React from "react";
 import { StyleSheet, Image } from "react-native";
 // import FastImage from "react-native-fast-image";
 
-const BackgroundImg = ({ img }) => {
+export default function BackgroundImg({ img }) {
   return <Image source={img} style={styles.backgroundImage} />;
-  // return (
-  //   <FastImage
-  //   style={styles.backgroundImage}
-  //     source={{
-  //       uri: { img },
-  //       priority: FastImage.priority.high,
-  //     }}
-  //     resizeMode={FastImage.resizeMode.contain}
-  //   />
-  // );
-};
-
-export default BackgroundImg;
+}
 
 const styles = StyleSheet.create({
   backgroundImage: {
