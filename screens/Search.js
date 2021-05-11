b91665121a2a4853fcd047375c96ab6e364ae7be
garyilayev <79512554+garyilayev@users.noreply.github.com>
2021-05-11 12:41:47 +0300

    Update Search.js
    Now reads from db instead of slicing email prefix.

--- screens/Search.js
+++ screens/Search.js
@@ -1,7 +1,10 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import Loading from '../components/Loading/Loading';
+import BackgroundImg from "../components/Background/BackgroundImg";
+import Avatar from "../components/Avatar/Avatar";
 import {
-  Button,
   View,
+  Image,
   Text,
   TextInput,
   StyleSheet,
@@ -11,15 +14,39 @@ import {
 import { StatusBar } from "expo-status-bar";
 // import DatePicker from "../components/DatePicker/DatePicker";
 import { AuthContext } from "../navigation/AuthProvider";
-import BackgroundImg from "../components/Background/BackgroundImg";
+import fb from "../fb";
 
-function Search({ navigation }) {
+function Search({ navigation}) {
   const { user, logout } = useContext(AuthContext);
+  const [data, setData] = useState({});
+  
+  const userInfo = fb.database().ref().child(`users/${user.uid}`);
+
+  useEffect(() => {
+    userInfo.once("value", function(snapshot) {
+      const info = snapshot.val();
+      const list = [];
+      for (let id in info) {
+        list.push( {id, ...info[id]} );
+      }
+      setData(list[0]);
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  },[])
+
   return (
     <View style={styles.container}>
       <BackgroundImg img={require("../assets/bgV2.jpg")} />
       <KeyboardAvoidingView behavior="padding">
         <StatusBar style="auto" />
+        <TouchableOpacity
+          style={styles.avatarContainer}
+          onPress={() => navigation.navigate('Profile')}
+        >
+         <Avatar/>
+        </TouchableOpacity>
+
         <View style={styles.header}>
           <Text
             style={{
@@ -29,7 +56,7 @@ function Search({ navigation }) {
               fontFamily: "Verdana",
             }}
           >
-            Welcome {user.email.slice(0, user.email.indexOf("@"))}
+            {data.firstName === undefined&& data.lastName === undefined  ? (<Loading/>) : ("Welcome "+data.firstName + " " + data.lastName)}{console.log(data)}
           </Text>
         </View>
         <View style={styles.inputContainer}>
@@ -57,6 +84,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
   },
+  avatarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30,
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    borderWidth: 2,
+    borderColor: "white",
+  },
   header: {},
   inputContainer: {
     alignItems: "center",
@@ -69,6 +109,12 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     opacity: 0.2,
     borderRadius: 7,
+  },
+  avatarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 30,
   },
   logOutButton: {
     width: 250,
