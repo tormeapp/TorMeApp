823f25772cf5ec7e5ab27dcbb850a6dbaf604ca8
garyilayev <79512554+garyilayev@users.noreply.github.com>
2021-05-11 12:48:28 +0300

    Update HomeStack.js
    Added User Profile inside HomeStack

--- navigation/HomeStack.js
+++ navigation/HomeStack.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import Search from '../screens/Search';
-//import { }
+import Profile from '../screens/Profile';
+
 const Stack = createStackNavigator();
 
 const HomeStack = () => {
     return ( 
         <Stack.Navigator>
             <Stack.Screen name='Search' component={Search} />
+            <Stack.Screen name="Profile" component={Profile} />
         </Stack.Navigator>
      );
 }
